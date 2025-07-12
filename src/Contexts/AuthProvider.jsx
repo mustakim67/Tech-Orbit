d79fa4d814cba351707cfc336a0ab97becb48cc1
mustakim67 <mahfujapple95@gmail.com>
2025-07-12 14:48:13 +0600

    Added firebase auth and context , auth provider and set up rreact router

---
+++ src/Contexts/AuthProvider.jsx
@@ -0,0 +1,15 @@
+import { AuthContext } from './AuthContext';
+
+const AuthProvider = ({ children }) => {
+   
+    const authInfo = {
+       
+    }
+    return (
+        <AuthContext value={authInfo}>
+            {children}
+        </AuthContext>
+    );
+};
+
+export default AuthProvider;
---
+++ src/Route/Router/Router.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {createBrowserRouter} from "react-router";
+import Home from '../../Pages/Home/Home/Home';
+import RootLayout from '../../Layouts/RootLayout';
+
+export const router = createBrowserRouter([
+    {
+      path:'/',
+      Component:RootLayout,
+      children:[
+        {
+            index:true,
+            Component:Home
+        }
+      ]
+    }
+]);
